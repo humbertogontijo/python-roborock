db5db6b82d43ae82fd6a4bf27f8707255f6408e2
CountGreven <32613942+CountGreven@users.noreply.github.com>
2023-05-31 19:25:02 +0200

    Order Dockerrorcodes numerically
    Reorders the error codes to appear in numerical order

--- roborock/code_mappings.py
+++ roborock/code_mappings.py
@@ -220,9 +220,9 @@ class RoborockDockErrorCode(RoborockEnum):
     """Describes the error code of the dock."""
 
     ok = 0
+    duct_blockage = 34
     water_empty = 38
     waste_water_tank_full = 39
-    duct_blockage = 34
 
 
 class RoborockDockTypeCode(RoborockEnum):
