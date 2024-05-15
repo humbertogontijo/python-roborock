d5a2e42f5622a2a96039e4716faaefc64868b30b
Luke <conway220@gmail.com>
2024-05-15 09:25:43 -0400

    fix: make cleanrecord handling a bit more robust

--- roborock/version_1_apis/roborock_client_v1.py
+++ roborock/version_1_apis/roborock_client_v1.py
@@ -198,7 +198,23 @@ class RoborockClientV1(RoborockClient):
             return CleanRecord.from_dict(record)
         elif isinstance(record, list):
             if isinstance(record[-1], dict):
-                return CleanRecord.from_dict(record[-1])
+                records = [CleanRecord.from_dict(rec) for rec in record]
+                final_record = records[-1]
+                try:
+                    # This code is semi-presumptions - so it is put in a try finally to be safe.
+                    final_record.begin = records[0].begin
+                    final_record.begin_datetime = records[0].begin_datetime
+                    final_record.start_type = records[0].start_type
+                    for rec in records[0:-1]:
+                        final_record.duration += rec.duration if rec.duration is not None else 0
+                        final_record.area += rec.area if rec.area is not None else 0
+                        final_record.avoid_count += rec.avoid_count if rec.avoid_count is not None else 0
+                        final_record.wash_count += rec.wash_count if rec.wash_count is not None else 0
+                        final_record.square_meter_area += (
+                            rec.square_meter_area if rec.square_meter_area is not None else 0
+                        )
+                finally:
+                    return final_record
             # There are still a few unknown variables in this.
             begin, end, duration, area = unpack_list(record, 4)
             return CleanRecord(begin=begin, end=end, duration=duration, area=area)
