6211a8163d130c41594daf65e36be2d87788a5c6
Luke Lashley <conway220@gmail.com>
2025-01-13 12:09:12 -0500

    fix: bump commitlint and allow caps (#283)
    * fix: bump commitlint and allow caps
    
    * fix: error

---
+++ commitlint.config.mjs
@@ -0,0 +1,9 @@
+export default {
+    extends: ["@commitlint/config-conventional"],
+    ignores: [(msg) => /Signed-off-by: dependabot\[bot]/m.test(msg)],
+    rules: {
+        // Disable the rule that enforces lowercase in subject
+        "subject-case": [0], // 0 = disable, 1 = warn, 2 = error
+    },
+
+};
